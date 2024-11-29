387ab77b75164b481068fe18d5674b94cadc87f1
pcxtx <119761872+pcxtx@users.noreply.github.com>
2024-11-29 15:11:24 +0200

    Create trport.sh

---
+++ extra/trport.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+trap 'exit 0' SIGTERM
+
+OLDPORT=0
+
+PORT=0
+
+while true
+do
+        [ -r "/pia-shared/port.dat" ] && PORT=$(cat /pia-shared/port.dat)
+
+        if [ $OLDPORT -ne $PORT ]; then
+                echo "Setting Transmission port to ($PORT)."
+                transmission-remote -p $PORT
+                OLDPORT=$PORT
+        fi
+
+        inotifywait --event modify --event create /pia-shared
+done
